bd054d105ac3e17227e1cc107d4decdb3e3d107a
Leonardo <leosimoes.engcomp@gmail.com>
2023-10-11 11:42:45 -0300

    Project started

---
+++ src/main/java/br/com/rocketseat/todolist/RocketseatJavaSpringRestApiToDoListApplication.java
@@ -0,0 +1,13 @@
+package br.com.rocketseat.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketseatJavaSpringRestApiToDoListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketseatJavaSpringRestApiToDoListApplication.class, args);
+    }
+
+}
